81781baa8300211f588b3d3ab64fd9568b544e71
Suraj Patil <you@example.com>
2023-12-14 16:31:04 +0530

    Inject ApplicationContext into singleton bean to behave Prototype Bean as Prototype scope

--- src/main/java/com/demo/springbootinterviewquestions/beanscope/SingletonBean.java
+++ src/main/java/com/demo/springbootinterviewquestions/beanscope/SingletonBean.java
@@ -1,19 +1,24 @@
 package com.demo.springbootinterviewquestions.beanscope;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SingletonBean {
+//    @Autowired
+//    private ProtoTypeBean protoTypeBean;
+
     @Autowired
-    private ProtoTypeBean protoTypeBean;
+    ApplicationContext context;
 
     public SingletonBean() {
         System.out.println("SingletonBean() instantiated !!");
     }
 
     public ProtoTypeBean getProtoTypeBean() {
-        return protoTypeBean;
+//        return protoTypeBean;
+        return context.getBean(ProtoTypeBean.class);
     }
 
 }
