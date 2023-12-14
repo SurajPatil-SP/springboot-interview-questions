8fc05ec1488fd5a14f439d31d92e6e703ba1280f
Suraj Patil <you@example.com>
2023-12-14 16:37:32 +0530

    Inject ObjectFactory into singleton bean to behave Prototype Bean as Prototype scope

--- src/main/java/com/demo/springbootinterviewquestions/beanscope/SingletonBean.java
+++ src/main/java/com/demo/springbootinterviewquestions/beanscope/SingletonBean.java
@@ -1,5 +1,6 @@
 package com.demo.springbootinterviewquestions.beanscope;
 
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
@@ -9,8 +10,10 @@ public class SingletonBean {
 //    @Autowired
 //    private ProtoTypeBean protoTypeBean;
 
+//    @Autowired
+//    ApplicationContext context;
     @Autowired
-    ApplicationContext context;
+    private ObjectFactory<ProtoTypeBean> objectFactory;
 
     public SingletonBean() {
         System.out.println("SingletonBean() instantiated !!");
@@ -18,7 +21,8 @@ public class SingletonBean {
 
     public ProtoTypeBean getProtoTypeBean() {
 //        return protoTypeBean;
-        return context.getBean(ProtoTypeBean.class);
+//        return context.getBean(ProtoTypeBean.class);
+        return objectFactory.getObject();
     }
 
 }
