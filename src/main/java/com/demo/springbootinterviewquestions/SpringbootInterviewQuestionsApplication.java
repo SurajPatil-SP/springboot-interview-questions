6fe7e3e92a3adb3fff9737894dda6cc2b08f5594
Suraj Patil <you@example.com>
2024-01-02 12:58:23 +0530

    Use of @PostConstruct and CommandLineRunner interface

--- src/main/java/com/demo/springbootinterviewquestions/SpringbootInterviewQuestionsApplication.java
+++ src/main/java/com/demo/springbootinterviewquestions/SpringbootInterviewQuestionsApplication.java
@@ -5,15 +5,32 @@ import com.demo.springbootinterviewquestions.beanscope.CustomThreadScope;
 import com.demo.springbootinterviewquestions.beanscope.SingletonBean;
 import com.demo.springbootinterviewquestions.common.Volunteer;
 import com.demo.springbootinterviewquestions.dependencyinjection.OrderService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.config.Scope;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class SpringbootInterviewQuestionsApplication {
+public class SpringbootInterviewQuestionsApplication implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("CommandLineRunner run() method ....... executed");
+    }
+
+    @PostConstruct
+    public void initLogic() {
+        System.out.println("PostConstruct logic executed ...!");
+        //connection pool logic
+        //kafka producer/consumer instantiate
+        //data shedding
+        //external API call
+    }
 
 	public static void main(String[] args) {
+        System.out.println("SpringApplication run() method ....... executed");
 		ConfigurableApplicationContext context = SpringApplication.run(SpringbootInterviewQuestionsApplication.class, args);
         OrderService service = context.getBean("orderService", OrderService.class);
 //        service.test();
@@ -42,5 +59,4 @@ public class SpringbootInterviewQuestionsApplication {
         BeanScopeTestService service3=context.getBean(BeanScopeTestService.class);
 */
 	}
-
 }
